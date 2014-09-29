f4424abb987ca4f02f4f2b2ba916e7358fd97b44
shellsh0ck <shellshock@fastmail.fm>
2014-09-29 18:04:36 +0600

    Initial Release
    Version: 1.0.0
    Status: Stable

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Pasan Premaratne on 9/16/14.
+//  Copyright (c) 2014 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
+    
+    let factBook = FactBook()
+    let colorWheel = ColorWheel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = factBook.randomFact()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showFunFact() {
+        var randomColor = colorWheel.randomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
+        funFactLabel.text = factBook.randomFact()
+    }
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# How to Build A Simple iPhone App with Swift
+
+Building the Fun Facts app will teach you how to use the Swift language and the Xcode and Interface Builder tools. You will also learn about core concepts such as views and view controllers, creating a data model and the Model-View-Controller design pattern. Towards the end you will have finished creating a fun app that will get you oriented with world of iOS development.
